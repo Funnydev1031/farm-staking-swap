8f59de0ee7e7f63bc60413ba6211e2ca8b6131f4
funnydev1031 <elvisjin1031@outlook.com>
2022-06-12 19:56:53 -0300

    first commit

---
+++ src/config/constants/farms.ts
@@ -0,0 +1,154 @@
+import tokens from './tokens'
+import { FarmConfig } from './types'
+
+const farms: FarmConfig[] = [
+  /**
+   * These 3 farms (PID 0, 1, 8 ) should always be at the top of the file we use them for price strings.
+   */
+  {
+    pid: 0,
+    lpSymbol: 'SCC',
+    lpAddresses: {
+      97: '',
+      56: '0xc26EaFC627624baDf990f8d30116892eD204DB51',
+    },
+    token: tokens.syrup,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 1,
+    lpSymbol: 'SCC-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x41496D9e7F1364dab33433d28962745FAaF6a577',
+    },
+    token: tokens.cake,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 2,
+    lpSymbol: 'SCC-BUSD LP',
+    lpAddresses: {
+      97: '',
+      56: '0xf04bc5e9EB85374DDa27DD06FEbD1fe2e0CBdB16',
+    },
+    token: tokens.cake,
+    quoteToken: tokens.busd,
+  },
+  {
+    pid: 8,
+    lpSymbol: 'BUSD-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
+  /* Add new farms below this line */
+  {
+    pid: 4,
+    lpSymbol: 'ETH-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x31c4f338FA35E8792C285b9FD2ccaC912F06b0D8',
+    },
+    token: tokens.eth,
+    quoteToken: tokens.cake,
+  }, 
+  {
+    pid: 5,
+    lpSymbol: 'SCC-SHIB LP',
+    lpAddresses: {
+      97: '',
+      56: '0xfe7be78a50a514b4390316a8e49d7574ba68ef64',
+    },
+    token: tokens.shib,
+    quoteToken: tokens.cake,
+  }, 
+  {
+    pid: 6,
+    lpSymbol: 'DOGE-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x2758e0C3d24E07Bf851fD04397243C3691ec2C38',
+    },
+    token: tokens.doge,
+    quoteToken: tokens.cake,
+  },
+  {
+    pid: 15,
+    lpSymbol: 'MWF-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x9BedF5B6eA1634d21607fcEC5c97c031d60c2123',
+    },
+    token: tokens.mwf,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 14,
+    lpSymbol: 'MWF-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0xF1bAAA261889953A354897BfA6632793058e3243',
+    },
+    token: tokens.mwf,
+    quoteToken: tokens.cake,
+  },  
+  {
+    pid: 7,
+    lpSymbol: 'DOGE-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0xac109C8025F272414fd9e2faA805a583708A017f',
+    },
+    token: tokens.doge,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 10,
+    lpSymbol: 'TEM-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1980357681Da77B2B91bB8037b487137ba5A5EDd',
+    },
+    token: tokens.tem,
+    quoteToken: tokens.cake,
+  },
+  {
+    pid: 11,
+    lpSymbol: 'KMZ-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x16C11F605F3CE6216feF17dF41b5DA682EC84c20',
+    },
+    token: tokens.kmz,
+    quoteToken: tokens.cake,
+  },
+  {
+    pid: 12,
+    lpSymbol: 'PRAISE-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x8c8D654706B4878436c8e245229F96dB4fa8807a',
+    },
+    token: tokens.praise,
+    quoteToken: tokens.cake,
+  },
+  {
+    pid: 13,
+    lpSymbol: 'LILX-SCC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x611A0fA0D2434c28bc2dF549629E75b7e58C4940',
+    },
+    token: tokens.lilx,
+    quoteToken: tokens.cake,
+  },
+   
+  
+  
+ ]
+
+ export default farms
---
+++ src/config/constants/pools.ts
@@ -0,0 +1,156 @@
+import tokens from './tokens'
+import { PoolConfig, PoolCategory } from './types'
+
+const pools: PoolConfig[] = [
+  {
+    sousId: 0,
+    stakingToken: tokens.cake,
+    earningToken: tokens.cake,
+    contractAddress: {
+      97: '',
+      56: '0x1080450b1e26Df071c613d916055b23e8FCc6Fca',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+    isFinished: false,
+  },
+  {
+    sousId: 1,
+    stakingToken: tokens.cake,
+    earningToken: tokens.shib,
+    contractAddress: {
+      97: '',
+      56: '0xa8567fa6A62bbB63DD512ECfF91df86249FE7090',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 2,
+    isFinished: true,
+    tokenPerBlock: '10',
+  },
+  {
+    sousId: 2,
+    stakingToken: tokens.cake,
+    earningToken: tokens.shib,
+    contractAddress: {
+      97: '',
+      56: '0x50fF88df5a0Bba734e8e405914e8D72F656BD301',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 3,
+    isFinished: true,
+    tokenPerBlock: '7',
+  },
+  {
+    sousId: 3,
+    stakingToken: tokens.cake,
+    earningToken: tokens.shib,
+    contractAddress: {
+      97: '',
+      56: '0x1DB1945A84748742093e77DB5B6FFc812aE16BB9',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 4,
+    tokenPerBlock: '7',
+  },
+  {
+    sousId: 4,
+    stakingToken: tokens.cake,
+    earningToken: tokens.tem,
+    contractAddress: {
+      97: '',
+      56: '0xC8c8C0e46e5E9d24c0ce6e3986D2F664Ce2b837B',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 5,
+    tokenPerBlock: '0.000002000',
+  },  
+  {
+    sousId: 5,
+    stakingToken: tokens.lilx,
+    earningToken: tokens.lilx,
+    contractAddress: {
+      97: '',
+      56: '0xd44755047848b686Dbc0dDFc59906934D9F767ba',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 6,
+    tokenPerBlock: '140000',
+  },  
+  {
+    sousId: 6,
+    stakingToken: tokens.fork,
+    earningToken: tokens.high,
+    contractAddress: {
+      97: '',
+      56: '0x60c4998C058BaC8042712B54E7e43b892Ab0B0c4',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 7,
+    tokenPerBlock: '0.09756',
+  },
+  {
+    sousId: 7,
+    stakingToken: tokens.fork,
+    earningToken: tokens.ceek,
+    contractAddress: {
+      97: '',
+      56: '0xED53944b1c0cEecDe1a413fDb4D0496e1a08ab58',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 8,
+    tokenPerBlock: '0.8078',
+  },
+  {
+    sousId: 8,
+    stakingToken: tokens.fork,
+    earningToken: tokens.tinc,
+    contractAddress: {
+      97: '',
+      56: '0x9593462fF51A14633b243Ba3d054A8183d057A02',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 9,
+    tokenPerBlock: '0.4677',
+  }, 
+  {
+    sousId: 9,
+    stakingToken: tokens.kmz,
+    earningToken: tokens.kmz,
+    contractAddress: {
+      97: '',
+      56: '0x2699549de69013F7F296981557682f3d080C9Fb7',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 10,
+    isFinished: true,
+    tokenPerBlock: '4975',
+  }, 
+{
+    sousId: 10,
+    stakingToken: tokens.cake,
+    earningToken: tokens.shib,
+    contractAddress: {
+      97: '',
+      56: '0xe63d3fe5e3476F1278e413C2AA5846372eC19df7',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 11,
+    tokenPerBlock: '7',
+  }, 
+
+
+ ]
+
+ export default pools
